46a171e5e8f4f89e1442daae5c4d951ad8423d28
t-oshima <t-oshima@nri.co.jp>
2020-01-02 16:06:36 +0900

    add java sample application by spring boot

---
+++ sample-java-jooq/src/main/java/com/example/samplejavajooq/SampleJavaJooqApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplejavajooq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleJavaJooqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleJavaJooqApplication.class, args);
+	}
+
+}
