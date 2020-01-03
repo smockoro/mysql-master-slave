07b30ac542bf5ef226a4234a8bcf64b1d90cc282
t-oshima <t-oshima@nri.co.jp>
2020-01-04 04:28:00 +0900

    commit update

--- sample-go/cmd/main.go
+++ sample-go/cmd/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/smockoro/mysql-master-slave/sample-go/adapter/repository"
+	"github.com/smockoro/mysql-master-slave/sample-go/component/config"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	cfg := config.NewConfig()
+	db, err := config.ConnectDB(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ur := repository.NewUserRepository(db)
+
+	ctx := context.Background()
+	user, err := ur.Find(ctx, 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(user)
+
+	os.Exit(0)
 }
--- sample-go/go.mod
+++ sample-go/go.mod
@@ -1,3 +1,9 @@
 module github.com/smockoro/mysql-master-slave/sample-go
 
 go 1.12
+
+require (
+	github.com/go-sql-driver/mysql v1.4.1
+	github.com/jmoiron/sqlx v1.2.0
+	google.golang.org/appengine v1.6.5 // indirect
+)
